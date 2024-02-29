9c1182b64253de2eb562acfb37931224b4eb8cc1
Fadwa <fadwa.fawzy@gmail.com>
2024-02-29 20:05:08 +0200

    Update missing fields

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -11,36 +11,24 @@ class ExtraInfoForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ExtraInfoForm, self).__init__(*args, **kwargs)
-        self.fields["nationality"].required = True
         self.fields["age"].required = True
-        self.fields["gender"].required = True
         self.fields["phone_number"].required = True
-        self.fields["qualification"].required = True
         self.fields["job_title"].required = True
 
     class Meta(object):
         model = ExtraInfo
         fields = (
-            "nationality",
             "age",
-            "gender",
             "phone_number",
-            "qualification",
             "job_title"
         )
         labels = {
-            "nationality": _("Nationality"),
             "age": _("Age"),
-            "gender": _("Gender"),
             "phone_number": _("Phone number"),
-            "qualification": _("Qualification"),
             "job_title": _("Job Title"),
         }
         help_text = {
-            "nationality": _("Please enter your Nationality"),
             "age": _("Please enter your Age"),
-            "gender": _("Please enter your gender"),
             "phone_number": _("Please enter your phone number"),
-            "qualification": _("Please enter your qualification"),
             "job_title": _("Please enter your job title"),
         }
