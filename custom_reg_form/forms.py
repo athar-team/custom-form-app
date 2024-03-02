db1f592893b6d77bf385c26352bacd325e2e7bef
Fadwa <fadwa.fawzy@gmail.com>
2024-03-02 23:46:32 +0200

    use forms.Form

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -1,10 +1,9 @@
 from django import forms
 from .models import UserFields
-from django.forms import ModelForm
 from django.utils.translation import gettext_lazy as _
 
 
-class UserFieldsForm(ModelForm):
+class UserFieldsForm(forms.Form):
     age = forms.IntegerField(required=True)
     mobile_number = forms.CharField(required=True)
     job_title = forms.CharField(required=True)
