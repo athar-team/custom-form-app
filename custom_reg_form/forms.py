085d153663bc53544bd64f24ee0587fbe3585f0e
Fadwa <fadwa.fawzy@gmail.com>
2024-03-03 01:15:15 +0200

    Fix form name

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -2,13 +2,13 @@ from .models import UserFields
 from django.forms import ModelForm
 
 
-class ExtraInfoForm(ModelForm):
+class UserFieldsForm(ModelForm):
     """
     The fields on this form are derived from the ExtraInfo model in models.py.
     """
 
     def __init__(self, *args, **kwargs):
-        super(ExtraInfoForm, self).__init__(*args, **kwargs)
+        super(UserFieldsForm, self).__init__(*args, **kwargs)
         self.fields["age"].error_messages = {
             "required": "Please tell us your favorite movie.",
             "invalid": "We're pretty sure you made that movie up.",
